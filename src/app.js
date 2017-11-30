c91ff412b75535c51ee84f984c5cbe2510434b2c
Laurent Grima <grima.laurent@gmail.com>
2017-11-30 23:38:55 +0100

    Implement basic search with algolia client

--- src/app.js
+++ src/app.js
@@ -3,10 +3,35 @@
 import React, { Component } from 'react';
 import './app.css';
 
-type Props = {};
+import algoliasearch from 'algoliasearch';
+// import algoliasearchHelper from 'algoliasearch-helper';
+const client = algoliasearch('AA6Z3N1QN6', '606fb361d72620af82ded9d61fd5ce9b');
+const index = client.initIndex('restaurants');
+// const helper = algoliasearchHelper(client, 'restaurants', { hitsPerPage: 5 });
 
-class App extends Component<Props> {
+type Props = {};
+type State = {
+  inputValue: string,
+  hits: Array<Object>,
+};
+
+class App extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { inputValue: '', hits: [] };
+  }
+
+  handleChange = e => {
+    const value = e.target.value;
+    this.setState({ inputValue: value });
+    index.search(value).then(results => {
+      this.setState({ hits: results.hits });
+      console.log(results.hits);
+    });
+  };
+
   render() {
+    const { inputValue, hits } = this.state;
     return (
       <div className="App">
         <div className="Container">
@@ -14,7 +39,10 @@ class App extends Component<Props> {
             <input
               className="SearchBar__Input"
               type="text"
+              autoComplete="off"
               placeholder="Search for Restaurants by Name, Cuisine, Location"
+              value={inputValue}
+              onChange={this.handleChange}
             />
           </header>
           <div className="Content">
@@ -31,6 +59,7 @@ class App extends Component<Props> {
             </section>
             <section className="Results">
               <h2 className="SectionTitle">34 Results found</h2>
+              <ul>{hits.map(hit => <li>{hit.name}</li>)}</ul>
             </section>
           </div>
         </div>
