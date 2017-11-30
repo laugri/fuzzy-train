13223bc8b76a78d1c1a48aa6920460c8b075a81d
Laurent Grima <grima.laurent@gmail.com>
2017-11-30 22:53:06 +0100

    Set quick and dirty look and feel

--- src/app.js
+++ src/app.js
@@ -7,7 +7,35 @@ type Props = {};
 
 class App extends Component<Props> {
   render() {
-    return <div className="App">Hello</div>;
+    return (
+      <div className="App">
+        <div className="Container">
+          <header className="SearchBar">
+            <input
+              className="SearchBar__Input"
+              type="text"
+              placeholder="Search for Restaurants by Name, Cuisine, Location"
+            />
+          </header>
+          <div className="Content">
+            <section className="FilterBar">
+              <section>
+                <h2 className="SectionTitle">Cuisine/Food type</h2>
+              </section>
+              <section>
+                <h2 className="SectionTitle">Rating</h2>
+              </section>
+              <section>
+                <h2 className="SectionTitle">Payment Options</h2>
+              </section>
+            </section>
+            <section className="Results">
+              <h2 className="SectionTitle">34 Results found</h2>
+            </section>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
